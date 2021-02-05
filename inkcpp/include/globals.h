6d69d6495f7bec0eb907257e7aabdb4d784eb9e6
Julian Benda <julian.benda@ovgu.de>
2021-02-05 08:10:14 +0100

    Add interface for access global variables

--- inkcpp/include/globals.h
+++ inkcpp/include/globals.h
@@ -11,8 +11,41 @@ namespace ink::runtime
 	class globals_interface
 	{
 	public:
-		// No public interface yet
-		virtual void dummy() = 0;
+
+		/**
+		 * @brief Access global variable of Ink runner.
+		 * @param name name of variable, as defined in InkScript
+		 * @tparam T c++ type of variable
+		 */
+		template<typename T>
+		T* get(const char* name);
+
 		virtual ~globals_interface() = default;
+
+	protected:
+		virtual uint32_t* getUInt(const char* name) = 0;
+		virtual int32_t* getInt(const char* name) = 0;
+		virtual float* getFloat(const char* name) = 0;
+		virtual char* getStr(const char* name) = 0;
 	};
+
+	template<>
+	uint32_t* globals_interface::get<uint32_t>(const char* name) {
+		return getUInt(name);
+	}
+
+	template<>
+	int32_t* globals_interface::get<int32_t>(const char* name) {
+		return getInt(name);
+	}
+
+	template<>
+	float* globals_interface::get<float>(const char* name) {
+		return getFloat(name);
+	}
+
+	template<>
+	char* globals_interface::get<char>(const char* name) {
+		return getStr(name);
+	}
 }
