6a09af560b13e778c65636a1210bdf8ed1f13f30
Julian Benda <julian.benda@ovgu.de>
2021-03-01 12:42:38 +0100

    Add definition of limits in config

--- shared/public/config.h
+++ shared/public/config.h
@@ -9,3 +9,16 @@
 
 // Only turn on if you have json.hpp and you want to use it with the compiler
 // #define INK_EXPOSE_JSON
+
+namespace ink::config {
+	static constexpr unsigned limitGlobalVariables = 50;
+	static constexpr unsigned limitThreadDepth = 20;
+	static constexpr unsigned limitEvalStackDepth = 20;
+	static constexpr unsigned limitContainerDepth = 20
+	// temporary variables and callstack;
+	static constexpr unsigned limitRuntimeStack = 50;
+	// max number of elements in one output (a string is one element)
+	static constexpr unsigned limitOutputSize = 200;
+	// max number of choices per choice
+	static constexpr unsigned maxChoices = 10;
+}
