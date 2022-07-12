4247ddc365adbd05e42b6518dcbf3cff726d825a
JBenda <julian.benda@ovgu.de>
2022-07-12 12:13:17 +0200

    Fix wrong distribution between new_lines (#45)
    https://github.com/brwarner/inkcpp/issues/44

---
+++ inkcpp_test/NewLines.cpp
@@ -0,0 +1,44 @@
+#include "catch.hpp"
+#include "../inkcpp_cl/test.h"
+
+#include <story.h>
+#include <runner.h>
+#include <compiler.h>
+
+using namespace ink::runtime;
+
+SCENARIO("a story has the proper line breaks", "[lines]")
+{
+	GIVEN("a story with line breaks")
+	{
+		inklecate("ink/LinesStory.ink", "LinesStory.tmp");
+		ink::compiler::run("LinesStory.tmp", "LinesStory.bin");
+		auto ink = story::from_file("LinesStory.bin");
+		runner thread = ink->new_runner();
+		WHEN("start thread")
+		{
+			THEN("thread can continue")
+			{
+				REQUIRE(thread->can_continue());
+			}
+			WHEN("consume lines")
+			{
+				std::string line1 = thread->getline();
+				std::string line2 = thread->getline();
+				std::string line3 = thread->getline();
+				std::string line4 = thread->getline();
+				THEN("lines are correct")
+				{
+					REQUIRE(line1 == "Line 1\n");
+					REQUIRE(line2 == "Line 2\n");
+					REQUIRE(line3 == "Line 3\n");
+					REQUIRE(line4 == "Line 4\n");
+				}
+				THEN("thread cannot continue")
+				{
+					REQUIRE(!thread->can_continue());
+				}
+			}
+		}
+	}
+}
