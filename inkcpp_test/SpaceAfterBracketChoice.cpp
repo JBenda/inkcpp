9c862cc4bda833a4997c57ade8e5da17020840a8
Julian Benda <julian.benda@ovgu.de>
2023-12-12 16:40:51 +0100

    Add new test based on https://github.com/brwarner/inkcpp/issues/63

---
+++ inkcpp_test/SpaceAfterBracketChoice.cpp
@@ -0,0 +1,48 @@
+#include "catch.hpp"
+#include "../inkcpp_cl/test.h"
+
+#include <story.h>
+#include <globals.h>
+#include <runner.h>
+#include <compiler.h>
+
+using namespace ink::runtime;
+
+SCENARIO("a story with bracketed choices and spaces can choose correctly", "[choices]")
+{
+	GIVEN("a story with line breaks")
+	{
+		inklecate("ink/ChoiceBracketStory.ink", "ChoiceBracketStory.tmp");
+		ink::compiler::run("ChoiceBracketStory.tmp", "ChoiceBracketStory.bin");
+		auto ink = story::from_file("ChoiceBracketStory.bin");
+		runner thread = ink->new_runner();
+		thread->getall();
+		WHEN("start thread")
+		{
+			THEN("thread has choices")
+			{
+				thread->getall();
+				REQUIRE(thread->has_choices());
+			}
+			WHEN("choose choice 1")
+			{
+				thread->choose(0);
+				thread->getall();
+				THEN("still has choices")
+				{
+					thread->getall();
+					REQUIRE(thread->has_choices());
+				}
+			}
+			WHEN("choose choice 2")
+			{
+				thread->choose(1);
+				thread->getall();
+				THEN("still has choices")
+				{
+					REQUIRE(thread->has_choices());
+				}
+			}
+		}
+	}
+}
---
+++ inkcpp_test/ink/ChoiceBracketStory.ink
@@ -0,0 +1,7 @@
+-> choices
+= choices
+
++	[Choice 1]
++	[Choice 2] 
+
+-	-> choices
