82aa75ff0891269b03f0f2a3c1482c4468751c92
Julian Benda <julian.benda@ovgu.de>
2023-12-17 22:54:56 +0100

    Add python exapmle

---
+++ inkcpp_py/example.py
@@ -0,0 +1,72 @@
+#!/bin/python
+
+import inkcpp_py
+import sys
+
+# load story and maybe snapshot
+story = inkcpp_py.Story.from_file('unreal_example.bin')
+if len(sys.argv) > 1:
+    snap = inkcpp_py.Snapshot.from_file(sys.argv[1])
+    globals = story.new_globals_from_snapshot(snap)
+    runner = story.new_runner_from_snapshot(snap, globals)
+else:
+    globals = story.new_globals()
+    runner = story.new_runner(globals)
+
+# access global variables
+print("Date: ", globals.date)
+globals.date = inkcpp_py.Value("17.12.2023")
+bg = globals.background.as_list();
+print(bg)
+bg.add('b')
+print(bg)
+bg.remove('a')
+print(bg)
+globals.background = inkcpp_py.Value(bg)
+
+# observer examples
+def ob_ping():
+    print("chang^-^")
+def ob_value(x):
+    print("now: ", x)
+def ob_delta(x, y):
+    print("from:",y,"to:",x)
+
+globals.observe_ping("brightness", ob_ping)
+globals.observe_value("brightness", ob_value)
+globals.observe_delta("brightness", ob_delta)
+
+
+# external function with no input, but return value
+def greeting(a):
+    return inkcpp_py.Value("Tach")
+runner.bind("GetGreeting", greeting)
+
+# external function with no return value
+def brightness(args):
+    print("Set Brightness: ", args[0])
+runner.bind_void("SetBrightness", brightness)
+
+# simple story stepper
+while True:
+    while runner.can_continue():
+        print(runner.getline())
+    print("# tags: ", end="")
+    print(', '.join(runner.tags()))
+    if runner.has_choices():
+        print()
+        index = 1
+        for c in runner:
+            print(str(index) + ": " + c.text())
+            print("\t"+', '.join(c.tags()))
+            index += 1
+        index = int(input('Select choice to continue: '))
+        if index == -1:
+            snap = runner.create_snapshot();
+            snap.write_to_file("story.snap");
+            break
+        else:
+            runner.choose(index - 1)
+    else:
+        break
+print("Finish")
