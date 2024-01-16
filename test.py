d99b081c1f9579934f2defb696c1a2fd9afc020f
JBenda <Zabuza98@gmx.net>
2024-01-16 13:29:50 +0100

    add test case for crash behaivior

---
+++ test.py
@@ -0,0 +1,28 @@
+import inkcpp_py as py
+
+story = py.Story.from_file("UE_example.ink.bin")
+glob = story.new_globals()
+run = story.new_runner(glob)
+
+def ob(new, old):
+    l = new.as_list()
+    print("P:")
+    for f in l.flags_from("Potions"):
+        print("\t", f.list_name, f.name)
+    print("C:")
+    for f in l.flags_from("Clues"):
+        print("\t", f.list_name, f.name)
+    
+
+glob.observe("Inventory", ob)
+
+while run.can_continue():
+    print(run.getline())
+
+run2 = story.new_runner(glob)
+
+print("\n\nRUN2\n")
+run2.move_to("TClues.TSkull")
+while run2.can_continue():
+    print(run2.getline())
+
