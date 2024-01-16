0086fc415be1644e806c08a99a47815d2d70b0d7
Julian Benda <julian.benda@ovgu.de>
2024-01-16 20:49:31 +0100

    new snapshot

--- inkcpp_py/tests/test_Snapshot.py
+++ inkcpp_py/tests/test_Snapshot.py
@@ -10,10 +10,12 @@ def check_end(runner):
 
 class TestSnapshot:
     def test_snapshot(self, assets, generate):
-        [story, _, runner] = generate(assets['SimpleStoryFlow'])
-
+        [story, glob, runner] = generate(assets['SimpleStoryFlow'])
+        runner2 = story.new_runner(glob)
         runner.getline()
         assert runner.num_choices() == 3
+        snap0 = runner.create_snapshot()
+
         runner.choose(2)
 
         snap1 = runner.create_snapshot()
@@ -27,14 +29,29 @@ class TestSnapshot:
 
         check_end(runner)
 
-        runner = story.new_runner_from_snapshot(snap1)
+        glob = story.new_globals_from_snapshot(snap0)
+        runner = story.new_runner_from_snapshot(snap0, glob, 1)
+        assert runner.num_choices() == 3
+        runner.choose(2)
+        cnt_x = 0
         while runner.can_continue():
             runner.getline()
-            cnt -= 1
-        assert cnt == 0
+            cnt_x += 1
+        assert cnt_x == cnt
 
         check_end(runner)
 
-        runner = story.new_runner_from_snapshot(snap2)
+        glob = story.new_globals_from_snapshot(snap1)
+        runner = story.new_runner_from_snapshot(snap1, glob, 1)
+        cnt_x = 0
+        while runner.can_continue():
+            runner.getline()
+            cnt_x += 1
+        assert cnt_x == cnt
+
+        check_end(runner)
+
+        glob = story.new_globals_from_snapshot(snap2)
+        runner = story.new_runner_from_snapshot(snap2, glob, 1)
         assert not runner.can_continue()
         check_end(runner)
