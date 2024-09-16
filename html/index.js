768daf29cd71922616cb3c84a8f31b1cc41f1977
JBenda <Zabuza98@gmx.net>
2024-09-16 14:39:41 +0000

    Deploying to inkproof-page from @ JBenda/inkcpp@8b85f2d39b65235c1f7675a4a6df8863b4418a67 🚀

--- html/index.js
+++ html/index.js
@@ -1,6 +1,7 @@
 var index =
 [
     [ "CMAKE usage", "index.html#cmake", [
+      [ "CMake Flags", "index.html#cmake_flags", null ],
       [ "main.cpp", "index.html#src_main", null ],
       [ "CMakeLists.txt", "index.html#src_cmake", null ],
       [ "test.ink", "index.html#src_story_json", null ]
