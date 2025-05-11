02c3f941e2050ee8fb2abe161457a85e2f39334a
Julian <lukasklause@gmail.com>
2025-05-11 17:24:11 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="inkcpp-py",
-    version="0.1.8",
+    version="0.1.9",
     author="Julian Benda",
     author_email="julian.benda@ovgu.de",
     description="Python bindings for InkCPP a Inkle runtime written in C++",
