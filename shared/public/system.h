e0edd71aec3491a20bd510fc8e6e19b926562c33
Harry Rose <harryrose@protonmail.com>
2025-12-14 15:32:50 +0000

    Wrap new fprintf, abort code in INK_ENABLE_CSTD

--- shared/public/system.h
+++ shared/public/system.h
@@ -215,20 +215,17 @@ void ink_assert(bool condition, const char* msg = nullptr, Args... args)
 			size_t size    = snprintf(nullptr, 0, msg, args...) + 1;
 			char*  message = static_cast<char*>(malloc(size));
 			snprintf(message, size, msg, args...);
-#	ifdef INK_ENABLE_EXCEPTIONS
-			throw ink_exception(message);
-#	else
-			fprintf(stderr, "Ink Assert: %s\n", message);
-			abort();
-#	endif
-		} else {
-#	ifdef INK_ENABLE_EXCEPTIONS
-			throw ink_exception(msg);
-#	else
-			fprintf(stderr, "Ink Assert: %s\n", msg);
-			abort();
-#	endif
+			msg = message;
 		}
+
+#	ifdef INK_ENABLE_EXCEPTIONS
+		throw ink_exception(msg);
+#	elif defined(INK_ENABLE_CSTD)
+		fprintf(stderr, "Ink Assert: %s\n", msg);
+		abort();
+#else
+	#error "This path needs a way to warn and then terminate, otherwise it'll silently fail"
+#	endif
 	}
 }
 
