20a26cb8d29c939da3126f1fc7f16497eb82b945
JBenda <Zabuza98@gmx.net>
2026-07-02 19:26:40 +0000

    Deploying to inkproof-page from @ JBenda/inkcpp@645f5a6a852c32a9b31979ab02dd6e0b9f344fc0 🚀

--- html/classink_1_1runtime_1_1globals__interface.js
+++ html/classink_1_1runtime_1_1globals__interface.js
@@ -1,21 +1,21 @@
 var classink_1_1runtime_1_1globals__interface =
 [
     [ "create_snapshot", "classink_1_1runtime_1_1globals__interface.html#ae9a415c16e7ed6e0a39422899823654d", null ],
+    [ "get", "classink_1_1runtime_1_1globals__interface.html#a7b1fc0f2c2592e4620a09e115ad09007", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#a31513db377a816112a082fc0ddd88ca7", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#a5970302d9613e5e26cefe65afeeeab62", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#af1c3c249e203b767b45712972abf24b6", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#a3586e8b4ac5600e9e2a0b617cb9b5f67", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#aaf845e44c6bcf50d19c09140a30a01e3", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#a9fb27c759f2fe443bb418e5f361e2934", null ],
-    [ "get", "classink_1_1runtime_1_1globals__interface.html#a605ffd8f7420f04f0df9fe63fef519e8", null ],
     [ "get", "classink_1_1runtime_1_1globals__interface.html#af1b554735287274f9687d8fdb01c60b1", null ],
     [ "observe", "classink_1_1runtime_1_1globals__interface.html#a0f1d8fe7859b28ee28986a14de19be61", null ],
+    [ "set", "classink_1_1runtime_1_1globals__interface.html#a4fa8e7ebe36c9a41a1f9c40a31dd8d99", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#a782a5adf49ca393204417cb028f4b11c", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#add52fabe248c933a8dcb95e2898d4a6d", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#a4e063af2ceb561a22fa78b0632710d37", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#a2aaf58a6b6f930ac8e958084e05c4065", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#ac5b0f6c0869e24e21757f5fe8b1df2ce", null ],
-    [ "set", "classink_1_1runtime_1_1globals__interface.html#a840bafebcf05f3ae0f4ff5f15a3a41ff", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#aaeb06800da97d4f1dd5f0a0d9ab63e75", null ],
     [ "set", "classink_1_1runtime_1_1globals__interface.html#a0277b08550382cdedf1aa3e43aef0291", null ],
     [ "statistics", "classink_1_1runtime_1_1globals__interface.html#ad32c8409bd61b265cc5a60440bc9d64d", null ]
