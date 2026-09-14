438df424c48e6a748696e7a5982625eb4a7a7908
JBenda <julian.m.benda@gmail.com>
2026-09-14 17:58:20 +0200

    fix(clib,lib): Shipe functionals libs now, with pkg-config (#172)
    + Windows assets now contain .lib files (again)
    + `.pc` (pkg-config) does now include C++ linkage (so that `gcc
    $(pkg-config ..) ` should work)
    + update documentaion
    + Removed `ink_compile_json` since it is currently not implemented
    (requires STL support)

--- inkcpp_c/include/inkcpp.h
+++ inkcpp_c/include/inkcpp.h
@@ -47,12 +47,12 @@ typedef struct HInkSTory    HInkStory;
 	 * page](https://github.com/JBenda/inkcpp/releases/latest) to `/MY/INKCPP/EXAMPLE_INSTALL/PATH`.
 	 * <br/> To run the example do the following:
 	 *
-	 * + change the `prefix=...` in `/MY/INKCPP/EXAMPLE_INSTALL/PATH/lib/pkgconfig/inkcpp.pc`
-	 *   to `prefix=/MY/INKCPP_EXAMPLE_INSTALL_PATH/`
 	 * + `export PKG_CONFIG_PATH=/MY/INKCPP/EXAMPLE_INSTALL/PATH/lib/pkgconfig`
-	 * + `gcc -c main.c $(pkg-config --cflags inkcpp)`
-	 * + `g++ -o main main.o $(pkg-config --libs inkcpp)`
+	 * + `clang main.c -o main $(pkg-config --define-prefix --cflags --libs inkcpp)`
 	 * + `./main`
+	 *
+	 * @note `clang` is used here since, unlike `gcc`, it is readily available on Windows, macOS and
+	 * Linux alike.
 	 *
 	 * As a sideproduct a file named `test.bin` should be created coaining the binary format used by
 	 * inkCPP.
@@ -455,18 +455,6 @@ typedef struct HInkSTory    HInkStory;
 	HInkRunner*  ink_story_new_runner_from_snapshot(
 	     HInkStory* self, const HInkSnapshot* obj, HInkGlobals* store, int runner_id
 	 );
-
-	/**
-	 * @ingroup clib
-	 * Compiles a .ink.json file to an inkCPP .bin file.
-	 * @param input_filename path to file contaning input data (.ink.json)
-	 * @param output_filename path to file output data will be written (.bin)
-	 * @param error if not NULL will contain a error message if an error occures (else will be set to
-	 * NULL)
-	 */
-	void
-	    ink_compile_json(const char* input_filename, const char* output_filename, const char** error);
-
 
 #ifdef __cplusplus
 }
